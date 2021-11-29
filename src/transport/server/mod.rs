9570ac973b791e315a3f78bc182530ba0fc19276
DmitryAstafyev <dvastafyev@gmail.com>
2021-11-29 08:42:08 +0100

    Update server trait

--- src/transport/server/mod.rs
+++ src/transport/server/mod.rs
@@ -4,7 +4,10 @@ use uuid::Uuid;
 
 pub type Sending = (Vec<u8>, Option<Uuid>);
 
-pub enum Events<E: std::error::Error> {
+pub trait Error: 'static + std::error::Error + Clone + Sync + Send {}
+
+#[derive(Clone, Debug)]
+pub enum Events<E: Error> {
     Ready,
     Shutdown,
     Connected(Uuid),
@@ -15,8 +18,24 @@ pub enum Events<E: std::error::Error> {
     ServerError(E),
 }
 
+impl<E: Error> std::fmt::Display for Events<E> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let output = match self {
+            Events::Ready => String::from("Ready"),
+            Events::Shutdown => String::from("Shutdown"),
+            Events::Connected(uuid) => format!("Connected({})", uuid),
+            Events::Disconnected(uuid) => format!("Disconnected({})", uuid),
+            Events::Received(uuid, buf) => format!("Received({}, {} bytes)", uuid, buf.len()),
+            Events::Error(uuid, err) => format!("Error({:?}): {}", uuid, err),
+            Events::ConnectionError(uuid, err) => format!("ConnectionError({:?}): {}", uuid, err),
+            Events::ServerError(err) => format!("ServerError({})", err),
+        };
+        write!(f, "{}", output)
+    }
+}
+
 #[async_trait]
-pub trait Control<E: std::error::Error>: Send + Clone {
+pub trait Control<E: Error>: Send + Clone {
     async fn shutdown(&self) -> Result<(), E>;
     async fn send(&self, buffer: Vec<u8>, client: Option<Uuid>) -> Result<(), E>;
     async fn disconnect(&self, client: Uuid) -> Result<(), E>;
@@ -24,7 +43,7 @@ pub trait Control<E: std::error::Error>: Send + Clone {
 }
 
 #[async_trait]
-pub trait Impl<E: std::error::Error + Clone, C: Control<E> + Send + Clone>: Send {
+pub trait Impl<E: Error + Clone, C: Control<E> + Send + Clone>: Send {
     async fn listen(&mut self) -> Result<(), E>;
     fn observer(&mut self) -> Result<UnboundedReceiver<Events<E>>, E>;
     fn control(&self) -> C;
